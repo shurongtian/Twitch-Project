d22b451cd10a60b294a03f8d5becb3dc8fd963be
ShurongTian <sxt151030@utdallas.edu>
2022-08-25 23:21:47 -0500

    Recommendation Logic Added

---
+++ src/main/java/com/thisisademo/demo1/service/RecommendationException.java
@@ -0,0 +1,7 @@
+package com.thisisademo.demo1.service;
+
+public class RecommendationException extends RuntimeException {
+    public RecommendationException(String errorMessage) {
+        super(errorMessage);
+    }
+}
